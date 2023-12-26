2620956ce582ef6f78e588eb40867aa2c0332531
규.이규민 <gmlee@lunasoft.co.kr>
2023-12-26 14:55:47 +0900

    메인 화면 api 연동작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import Link from 'next/link'
+import { IPost } from '@/types'
 
-const test_data = [1, 2, 3, 4, 5, 6, 7]
-
-export default function Home() {
+export default async function Home() {
+    const res = await fetch('http://localhost:9999/posts', {cache: 'no-store'})
+    const post_list: IPost[] = await res.json()
     return (
         <div className='py-4'>
             <h2 className='text-xl mb-3'>My Posts</h2>
             <ul className='grid grid-cols-3 gap-2'>
                 {
-                    test_data.map(n => (
-                        <li className='h-[261px] bg-black flex'>
-                            <Link href={`/post/${n}`} scroll={false} className='w-full h-full text-white text-4xl'>{ n }</Link>
+                    post_list.map(post => (
+                        <li className='h-[261px] bg-black flex' key={post.id}>
+                            <Link href={`/post/${post.id}`} scroll={false} className='w-full h-full text-white text-4xl'>
+                                <img className='object-cover aspect-square' src={post.imageSrc} alt="post image" />
+                            </Link>
                         </li>
                     ))
                 }
