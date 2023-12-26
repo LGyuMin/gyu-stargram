c5e323c9a9fdf9f9a06a3cb0b184dfc614fe3933
규.이규민 <gmlee@lunasoft.co.kr>
2023-12-26 14:28:10 +0900

    모달 페이지 스타일 작업

--- src/app/@modal/(.)post/[id]/page.tsx
+++ src/app/@modal/(.)post/[id]/page.tsx
@@ -3,7 +3,10 @@ import Modal from '@/components/Modal'
 export default function page({ params }: { params: {id: string} }) {
     return (
         <Modal>
-            <div className='text-4xl text-white'>{ params.id }</div>
+            <div className='flex gap-4 w-[800px] bg-white mx-auto'>
+                <div className='w-7/12 h-[466px] flex justify-center items-center bg-slate-400'>{ params.id }</div>
+                <div className='w-auto py-4'>글 내용 내용</div>
+            </div>
         </Modal>
     )
 }
